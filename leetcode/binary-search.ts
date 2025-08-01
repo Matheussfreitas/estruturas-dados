b42d10c4af09ff03ef83022d357fc50ad0ff4036
Matheus Freitas <mgsoutodelira@gmail.com>
2025-08-01 11:59:50 -0300

    up

---
+++ leetcode/binary-search.ts
@@ -0,0 +1,28 @@
+interface binarySearchProps {
+  nums: number[]
+  elemento: number
+}
+
+function binarySearch({nums, elemento}: binarySearchProps) {
+  let low = 0
+  let high = nums.length
+  let steps = 0
+
+  while (low < high) {
+    steps++
+    const mid = Math.floor((low + high) / 2)
+    
+    if (nums[mid] === elemento) {
+      console.log(`Elemento encontrado em ${steps} passos`)
+      return mid
+    } else if (nums[mid] < elemento) {
+      // define o ponteiro sendo o primeiro item da metade direita
+      low = mid + 1
+    } else {
+      // define o ponteiro sendo o último item da metade esquerda
+      high = mid
+    }
+  }
+  console.log(`Elemento não encontrado em ${steps} passos`)
+  return -1
+}
---
+++ leetcode/sliding.window.ts
@@ -0,0 +1,24 @@
+function maxLengthSubstring(s: string): number {
+  let left = 0;
+  let right = 0;
+  let max = 1;
+  const map = new Map<string, number>();
+
+  map.set(s[0], 1);
+
+  while (right < s.length - 1) {
+    right++;
+    if (map.has(s[right])) {
+      map.set(s[right], map.get(s[right])! + 1);
+    } else {
+      map.set(s[right], 1);
+    }
+
+    while (map.get(s[right])! === 3) {
+      map.set(s[left], map.get(s[left])! - 1);
+      left++;
+    }
+    max = Math.max(max, left - right + 1);
+  }
+  return max;
+}
